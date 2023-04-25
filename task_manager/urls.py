eb130ad957b53ef8aa7665d8b1647c9d0284f0c3
Amjith Titus <amjithtitus09@gmail.com>
2023-04-26 02:32:14 +0530

    Submission

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,8 +1,40 @@
 from django.contrib import admin
 from django.urls import path
 
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+
+tasks = []
+completed_tasks = []
+
+def tasks_view(request):
+    return render(request, "tasks.html", {"tasks" : tasks})
+
+def completed_tasks_view(request):
+    return render(request, "completed_tasks.html", {"completed_tasks" : completed_tasks})
+
+def all_tasks_view(request):
+    return render(request, "all_tasks.html", {"completed_tasks" : completed_tasks, "tasks" : tasks})
+
+def add_task_view(request):
+    tasks.append(request.GET.get("task"))
+    return HttpResponseRedirect("/tasks")
+
+def delete_task_view(request, index):
+    print("Delete Task")
+    del tasks[index-1]
+    return HttpResponseRedirect("/tasks")
+
+def complete_task_view(request, index):
+    completed_tasks.append(tasks.pop(index-1))
+    return HttpResponseRedirect("/tasks")
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    # Add all your views here
+    path('admin/', admin.site.urls),
+    path('tasks/', tasks_view),
+    path('add-task/', add_task_view),
+    path('delete-task/<int:index>/', delete_task_view),
+    path('complete_task/<int:index>/', complete_task_view),
+    path('completed_tasks/', completed_tasks_view),
+    path('all_tasks/', all_tasks_view),
 ]
